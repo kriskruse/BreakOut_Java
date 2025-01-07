401f934f623af5b065a215f276d7e34b25da02d7
Kris Kruse <kriskruse@hotmail.com>
2025-01-07 10:35:47 +0100

    maven with javaFX

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.breakout {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.ikonli.javafx;
+    requires com.almasb.fxgl.all;
+
+    opens dk.group12.breakout to javafx.fxml;
+    exports dk.group12.breakout;
+}
