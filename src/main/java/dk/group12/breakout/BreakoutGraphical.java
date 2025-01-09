777524dd15c647a1e9ac9d22b432cdd3f5b99397
ThatAugustus <s245764@dtu.dk>
2025-01-09 13:20:31 +0100

    main() with launch(args) added

--- src/main/java/dk/group12/breakout/BreakoutGraphical.java
+++ src/main/java/dk/group12/breakout/BreakoutGraphical.java
@@ -20,6 +20,10 @@ public class BreakoutGraphical extends Application {
     private final int windowY = 700;
     private GameLoop gameLoop;
 
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     // Track pressed keys
     private Set<String> activeKeys = new HashSet<>();
 
