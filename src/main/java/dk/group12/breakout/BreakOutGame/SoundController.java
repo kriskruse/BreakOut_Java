0d3db8ed02faab8a0e4d58789091478b8099f346
Kris Kruse <kriskruse@hotmail.com>
2025-01-13 12:49:16 +0100

    sounds initial

---
+++ src/main/java/dk/group12/breakout/BreakOutGame/SoundController.java
@@ -0,0 +1,47 @@
+package dk.group12.breakout.BreakOutGame;
+
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
+
+public class SoundController {
+    private static MediaPlayer pingPlayer;
+    private static MediaPlayer musicPlayer;
+    private static Media pingSound;
+    private static Media musicSound;
+    private static AudioClip pingTone;
+
+    private static final String pingFile = "src/main/resources/dk/group12/breakout/sounds/punch.wav";
+    private static final String musicFile = "src/main/resources/dk/group12/breakout/sounds/music.mp3";
+
+    public static boolean soundControl = true;
+
+    public SoundController() {
+        pingSound = new Media(new File(pingFile).toURI().toString());
+
+
+        musicSound = new Media(new File(musicFile).toURI().toString());
+        musicPlayer = new MediaPlayer(musicSound);
+        musicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        musicPlayer.setVolume(0.6);
+
+    }
+    public static void playPing() {
+        if (soundControl) {
+           pingPlayer = new MediaPlayer(pingSound);
+           pingPlayer.play();
+        } else {
+            pingTone.stop();
+        }
+    }
+
+    public void playMusic() {
+        if (soundControl) {
+            musicPlayer.play();
+        }else {
+            musicPlayer.stop();
+        }
+    }
+}
