e4c15ff9adc1637a209b1252a7e998abb76b0624
Kris Kruse <kriskruse@hotmail.com>
2025-01-14 10:39:30 +0100

    added randomized hit sounds and removed unused

--- src/main/java/dk/group12/breakout/BreakOutGame/SoundController.java
+++ src/main/java/dk/group12/breakout/BreakOutGame/SoundController.java
@@ -7,33 +7,36 @@ import javafx.scene.media.MediaPlayer;
 import java.io.File;
 
 public class SoundController {
-    private static MediaPlayer pingPlayer;
     private static MediaPlayer musicPlayer;
-    private static Media pingSound;
-    private static Media musicSound;
-    private static AudioClip pingTone;
+    private static final Media[] bounceSounds = new Media[5];
 
-    private static final String pingFile = "src/main/resources/dk/group12/breakout/sounds/beep.wav";
     private static final String musicFile = "src/main/resources/dk/group12/breakout/sounds/music.mp3";
+    private static final String[] bounceFiles = {
+            "src/main/resources/dk/group12/breakout/sounds/bounce1.wav",
+            "src/main/resources/dk/group12/breakout/sounds/bounce2.wav",
+            "src/main/resources/dk/group12/breakout/sounds/bounce3.wav",
+            "src/main/resources/dk/group12/breakout/sounds/bounce4.wav",
+            "src/main/resources/dk/group12/breakout/sounds/bounce5.wav"};
 
     public static boolean soundControl = true;
 
     public SoundController() {
-        pingSound = new Media(new File(pingFile).toURI().toString());
+
+        for (int i = 0; i < 5; i++) {
+            bounceSounds[i] = new Media(new File(bounceFiles[i]).toURI().toString());
+        }
 
 
-        musicSound = new Media(new File(musicFile).toURI().toString());
+        Media musicSound = new Media(new File(musicFile).toURI().toString());
         musicPlayer = new MediaPlayer(musicSound);
         musicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        musicPlayer.setVolume(0.6);
+        musicPlayer.setVolume(0.1);
 
     }
     public static void playPing() {
         if (soundControl) {
-           pingPlayer = new MediaPlayer(pingSound);
-           pingPlayer.play();
-        } else {
-            pingTone.stop();
+            MediaPlayer bouncePlayer = new MediaPlayer(bounceSounds[(int) (Math.random() * 5)]);
+            bouncePlayer.play();
         }
     }
 
